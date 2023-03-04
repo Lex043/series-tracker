858c16f1a7cefe2521e5fbab4942e1341a2861a6
Oyebamiji Alex <alexferanmi390@gmail.com>
2023-03-04 10:26:12 +0100

    feat: doings

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import uuid from "react-uuid";
 
 interface Results {
-  id: number;
+  id: string;
   title: string;
   season: number;
   episode: number;
@@ -18,17 +18,64 @@ const Form = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
     const obj = {
       title: text,
       id: uuid(),
       season: 0,
       episode: 0,
     };
+
     setSeries((current): any => [obj, ...current]);
+
     setText("");
   };
 
-  console.log(series);
+  const increaseSeason = (id: string) => {
+    setSeries((current: any) =>
+      current.map((item: Results) =>
+        item.id === id ? { ...item, season: item.season + 1 } : item
+      )
+    );
+  };
+
+  const decreaseSeason = (id: string) => {
+    setSeries((current: any) =>
+      current.map((item: Results) =>
+        item.id === id
+          ? {
+              ...item,
+              season: item.season <= 0 ? item.season : item.season - 1,
+            }
+          : item
+      )
+    );
+  };
+
+  const increaseEpisode = (id: string) => {
+    setSeries((current: any) =>
+      current.map((item: Results) =>
+        item.id === id ? { ...item, episode: item.episode + 1 } : item
+      )
+    );
+  };
+
+  const decreaseEpisode = (id: string) => {
+    setSeries((current: any) =>
+      current.map((item: Results) =>
+        item.id === id
+          ? {
+              ...item,
+              episode: item.episode <= 0 ? item.episode : item.episode - 1,
+            }
+          : item
+      )
+    );
+  };
+
+  const deleteSeries = (id: string) => {
+    setSeries((current) => current.filter((item: Results) => item.id !== id));
+  };
 
   return (
     <section>
@@ -45,14 +92,55 @@ const Form = () => {
         </button>
       </form>
 
-      {series &&
-        series.map((serie: Results) => (
-          <div key={serie.id}>
-            <p>{serie.title.toUpperCase()}</p>
-            <p>Season {serie.season}</p>
-            <p>Episode {serie.episode}</p>
-          </div>
-        ))}
+      <section className="flex flex-wrap gap-10 mx-6 mt-16">
+        {series &&
+          series.map((serie: Results) => (
+            <div
+              key={serie.id}
+              className="bg-white border-2 shadow-[#f0f0f0] shadow-sm py-6 px-4 rounded-md"
+            >
+              <p
+                onClick={() => deleteSeries(serie.id)}
+                className="text-end pb-6 cursor-pointer"
+              >
+                X
+              </p>
+              <div className="flex flex-col gap-2 items-center">
+                <p className="font-mono">{serie.title.toUpperCase()}</p>
+                <div className="flex items-center gap-2 py-2">
+                  <button
+                    onClick={() => decreaseSeason(serie.id)}
+                    className="bg-[#4a4a4a91] font-semibold text-white rounded-full w-7 h-7"
+                  >
+                    -
+                  </button>
+                  <p>Season {serie.season}</p>
+                  <button
+                    onClick={() => increaseSeason(serie.id)}
+                    className="bg-[#4a4a4a91] font-semibold text-white rounded-full w-7 h-7"
+                  >
+                    +
+                  </button>
+                </div>
+                <div className="flex items-center gap-2 py-2">
+                  <button
+                    onClick={() => decreaseEpisode(serie.id)}
+                    className="bg-[#4a4a4a91] font-semibold text-white rounded-full w-7 h-7"
+                  >
+                    -
+                  </button>
+                  <p>Episode {serie.episode}</p>
+                  <button
+                    onClick={() => increaseEpisode(serie.id)}
+                    className="bg-[#4a4a4a91] font-semibold text-white rounded-full w-7 h-7"
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </section>
     </section>
   );
 };
